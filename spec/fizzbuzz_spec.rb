0888a0c9a770038786ddaaf79d96911e8079c1dd
valeria campilongo <cvaleriac@hotmail.it>
2018-03-07 17:24:03 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
-
+String fizz_3
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     string fizz_3 == fizzbuzz(3)
