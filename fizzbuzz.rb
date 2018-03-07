92e58ced26a689d7d2e7e37887574cf7a99b2a10
valeria campilongo <cvaleriac@hotmail.it>
2018-03-07 17:17:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@ def fizzbuzz(value)
   temp = value.to_i
   if temp % 3 == 0
       puts "Fizz"
+    end
   #  elsif temp % 5 == 0
   #    puts "Buzz"
   #  elsif int % 3 && 5 == 0
   #    puts "FizzBuzz"
   #  else int !% 3 && 5 == 0
   #    puts "nil"
-  end
+end
