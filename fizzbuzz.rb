a5a1d7840d2b425d0819bc0412d0f6a35a442021
valeria campilongo <cvaleriac@hotmail.it>
2018-03-07 16:57:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
       puts "Fizz"
+    elsif int % 5 == 0
+      puts "Buzz"
+    elsif int % 3 && 5 == 0
+      puts "FizzBuzz"
+    else int !% 3 && 5 == 0
 end
